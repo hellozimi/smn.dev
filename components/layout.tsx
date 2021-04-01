1a34e9aeda214c507d24a3c68e63c83f1bc33fc2
Simon Andersson <simon@hiddencode.me>
2021-04-01 09:43:34 +0200

    adds font and fontsize to per/code

--- components/layout.tsx
+++ components/layout.tsx
@@ -23,6 +23,10 @@ const Layout: FunctionComponent<LayoutProps> = ({ children, home, til, title, mo
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
           rel="stylesheet"
         ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap"
+          rel="stylesheet"
+        ></link>
         <meta name="description" content={excerpt || `I'm Simon and I write code.`} />
         <meta name="og:title" content={title ? `${title} - smn.dev` : siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
